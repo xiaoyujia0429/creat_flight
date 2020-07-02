250ba74e44f94c2b432c566381b23b6eb262335c
zyj <2431511072@qq.com>
2020-07-02 09:23:04 +0800

    login的更改

--- src/views/login.vue
+++ src/views/login.vue
@@ -107,7 +107,7 @@ export default {
             this.$router.push({
               path: "/login"
             });
-          }    
+          }
           // this.$store
           //   .dispatch("Login", this.loginForm)
           //   .then(() => {
